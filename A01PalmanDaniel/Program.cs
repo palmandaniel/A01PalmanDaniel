46101716d39c4efd67d88f6efdf8d174d6eb2d53
palmandaniel <palman.daniel47@gmail.com>
2020-09-14 11:04:46 +0200

    rossz Beolvasas()

--- A01PalmanDaniel/Program.cs
+++ A01PalmanDaniel/Program.cs
@@ -3,14 +3,44 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace A01PalmanDaniel
 {
     class Program
     {
+        static void Beolvasas()
+        {
+            StreamReader sr = new StreamReader("adatok.dat");
+            while (!sr.EndOfStream)
+            {
+                string[] sor = sr.ReadLine().Split(' ');
+                int[] adatok = new int[1000];
+
+                for (int i = 0; i < adatok.Length; i++)
+                {
+                    adatok[i] = int.Parse(sor[i]);
+                }
+                int[] adat = new int[1000];
+                for (int i = 0; i < adatok.Length; i++)
+                {
+                    adat[i] = adatok[i] * 2;
+                    Console.WriteLine("{0}",adat);
+                }
+                
+
+            }
+            sr.Close();
+        }
+        static void Maximumertek()
+        { 
+            
+        }
         static void Main(string[] args)
         {
-
+            Beolvasas();
+            Maximumertek();
+            Console.ReadKey();
 
         }
     }
